10d17bfbf3410db1a07aa8403f383e26a759126c
Frostbourn <frostbourn@gmail.com>
2020-12-10 23:25:26 +0100

    Prepare to main build

--- src/App.js
+++ src/App.js
@@ -1,211 +1,78 @@
-import React from "react";
-import SimpleReactLightbox, { SRLWrapper } from "simple-react-lightbox";
+import React, { useState, useEffect } from "react";
+import { createApi, toJson } from "unsplash-js";
+
 import "./App.scss";
+import Header from "./components/Header";
+import SearchForm from "./components/SearchForm";
+import Sentence from "./components/Sentence";
+import Photos from "./components/Photos";
 
-export default class AutoCompletedText extends React.Component {
-  myRef = React.createRef();
+const unsplash = createApi({
+  accessKey: "8ba7daec662eb3e3f18f31a571b61faece5beb250fe546925e79e21ca827672f"
+});
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      suggestions: [],
-      text: "",
-      results: [],
-      showSuggestions: false
-    };
-  }
+const cache = JSON.parse(localStorage.getItem("cache")) || {};
+localStorage.clear();
 
-  componentDidMount() {
-    document.addEventListener("mousedown", this.handleClickOutside);
-    document.addEventListener("keydown", this.handleInputClick);
-  }
+const App = () => {
+  const [query, setQuery] = useState("funny");
+  const [photosData, setPhotosData] = useState({});
+  const [page, setPage] = useState(1);
 
-  componentWillUnmount() {
-    document.removeEventListener("mousedown", this.handleClickOutside);
-    document.removeEventListener("keydown", this.handleInputClick);
-  }
+  const fetchPhotos = () => {
+    if (cache[query] && cache[query][page]) {
+      setPhotosData(cache[query][page]);
+      // console.log(cache);
+    } else {
+      try {
+        unsplash.search
+          .getPhotos({
+            query: query,
+            page: 1,
+            per_page: 10
+          })
+          .then((result) => {
+            const results = result.response;
+            setPhotosData(results);
 
-  getImages = () => {
-    let { text } = this.state;
-    fetch(
-      `https://api.unsplash.com/search/photos?per_page=30&client_id=8ba7daec662eb3e3f18f31a571b61faece5beb250fe546925e79e21ca827672f&query=${text}`
-    )
-      .then((response) => response.json())
-      .then((data) => {
-        this.setState({
-          results: data.results
-        });
-      });
-  };
-
-  getSuggestions = async () => {
-    let { text } = this.state;
-    fetch(`https://api.datamuse.com/sug?s=${text}&max=6`)
-      .then((response) => response.json())
-      .then((data) => {
-        const suggestionsArray = [].concat(...data);
-        this.setState({
-          suggestions: suggestionsArray
-        });
-      });
-  };
-
-  onInputChange = (e) => {
-    const value = e.target.value;
-    let suggestions = [e.target.suggestions];
-    if (value.length > 2) {
-      const regex = new RegExp(`^${value}`, "i");
-      suggestions = suggestions.sort().filter((v) => regex.test(v));
-      this.getSuggestions();
-    }
-    this.setState(() => ({
-      suggestions,
-      text: value
-    }));
-  };
-
-  handleClickOutside = (e) => {
-    if (this.myRef.current && !this.myRef.current.contains(e.target)) {
-      this.setState({
-        showSuggestions: false
-      });
-    }
-  };
-
-  handleInputClick = (e) => {
-    this.setState((state) => {
-      return {
-        showSuggestions: !state.showSuggestions
-      };
-    });
-    if (e.key === "Enter") {
-      this.getImages();
-    }
-  };
-
-  selectedText(value) {
-    this.setState(() => ({ text: value }), this.getImages);
-  }
-
-  renderSuggestions = () => {
-    let { suggestions } = this.state;
-    if (suggestions.length < 2) {
-      return (
-        <>
-          <ul>
-            <li style={{ fontStyle: "italic", fontWeight: "bold" }}>
-              No suggestions
-            </li>
-          </ul>
-        </>
-      );
-    } else if (suggestions.length > 2) {
-      return (
-        <>
-          <ul>
-            {suggestions.map((item, index) => (
-              <li key={index} onClick={() => this.selectedText(item.word)}>
-                <a href="#top">{item.word}</a>
-              </li>
-            ))}
-            <li style={{ fontStyle: "italic", fontWeight: "bold" }}>
-              Suggestions: {suggestions.length}
-            </li>
-          </ul>
-        </>
-      );
-    }
-  };
-
-  render() {
-    const { text, results } = this.state;
-
-    const lightboxOptions = {
-      settings: {
-        disableKeyboardControls: true,
-        disableWheelControls: true
-      },
-      buttons: {
-        showAutoplayButton: false,
-        showDownloadButton: false,
-        showFullscreenButton: false,
-        showThumbnailsButton: false
-      },
-      thumbnails: {
-        showThumbnails: false
+            cache[query] = cache[query] ? cache[query] : {};
+            cache[query][page] = results;
+            localStorage.setItem("cache", JSON.stringify(cache));
+          });
+      } catch (err) {
+        console.error(err);
       }
-    };
+    }
+  };
 
-    return (
-      <div className="app">
-        <header className="app-header">
-          <div className="search-form">
-            <p className="logo">Unsplash</p>
-            <p className="sub-logo">
-              The internet’s source of <a href="#top">freely-usable images</a>.{" "}
-              <br />
-              Powered by creators everywhere.
-            </p>
-            <div ref={this.myRef}>
-              <input
-                id="query"
-                type="text"
-                onChange={this.onInputChange}
-                value={text}
-                onKeyPress={this.handleInputClick}
-                onClick={this.handleInputClick}
-              />
-              {this.state.showSuggestions && this.renderSuggestions()}
-            </div>
-          </div>
-        </header>
-        <SimpleReactLightbox>
-          <SRLWrapper options={lightboxOptions}>
-            <div className="app-sentence">
-              <svg
-                width="32"
-                height="32"
-                className="_1Ig-9"
-                version="1.1"
-                viewBox="0 0 32 32"
-                aria-hidden="false"
-              >
-                <path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path>
-              </svg>
-              <br />
-              <p>Make something awesome</p>
-            </div>
-            <main className="app-gallery">
-              {results.map((item) => {
-                return (
-                  <div key={item.id} className="item">
-                    <img
-                      src={item.urls.regular}
-                      alt={"Author: " + item.user.name}
-                    />
-                    <div className="item-caption">
-                      <p className="item-likes">
-                        <span role="img" aria-label="heart emoji">
-                          ❤️
-                        </span>{" "}
-                        {item.likes}
-                      </p>
-                      <a
-                        href={item.links.download}
-                        className="item-button"
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        Download
-                      </a>
-                    </div>
-                  </div>
-                );
-              })}
-            </main>
-          </SRLWrapper>
-        </SimpleReactLightbox>
+  const onSearchPhoto = (searchTerms) => {
+    setQuery(searchTerms);
+  };
+
+  useEffect(() => {
+    fetchPhotos();
+  }, [query, page]);
+
+  const photosArray = photosData.results || [];
+  return (
+    <div className="app">
+      <div className="app-header">
+        <div className="search-form">
+          <Header />
+          <SearchForm onSearchPhoto={onSearchPhoto} />
+        </div>
       </div>
-    );
-  }
-}
+      <Sentence />
+      <p>
+        Currently showing <b>{photosData.total}</b> results for: {""}
+        <b className="query">{query}</b>
+      </p>
+      <div className="app-gallery">
+        <Photos photos={photosArray} />
+        <button className="button">Load more</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
