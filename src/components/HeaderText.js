5eb28d0ffa04c42deedbc78b7c8f7d1e3a6459c1
Frostbourn <frostbourn@gmail.com>
2021-02-08 12:35:41 +0100

    Fixed deployment errors & some displaying issues

--- src/components/HeaderText.js
+++ src/components/HeaderText.js
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <>
-      <p className="form__logo">Free Photo Stock</p>
+      <p className="form__logo">Free Stock Photos</p>
       <p className="form__text">
         The internet’s source of <a href="#top">freely-usable images</a>. <br />
         Powered by creators everywhere.
